0444b526004c0cd3fd366c6115fd9b9b453dec45
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2024-02-15 14:08:39 +0500

    Реализована десктопная версия хедера

--- app/js/burger.js
+++ app/js/burger.js
@@ -13,22 +13,3 @@ toggle.addEventListener('click', () => {
   menu.classList.toggle('header__menu--opened');
   menu.classList.toggle('header__menu--closed');
 });
-
-function closeBurgerMenu() {
-  burgerList.classList.toggle('header__navigation--opened');
-  burgerList.classList.toggle('header__navigation--closed');
-  toggle.classList.toggle('header__toggle--opened');
-  toggle.classList.toggle('header__toggle--closed');
-  logo.classList.toggle('header__logo--opened');
-  logo.classList.toggle('header__logo--closed');
-  menu.classList.toggle('header__menu--opened');
-  menu.classList.toggle('header__menu--closed');
-  }
-
-  function handleResize() {
-    if (window.innerWidth <= 1366) {
-      closeBurgerMenu();
-    }
-  }
-  
-  window.addEventListener('resize', handleResize);
